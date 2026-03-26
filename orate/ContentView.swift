d01159b99889319bcb464ec4d6c0f5b9050898bc
Lavish Thakkar <lavisht22@gmail.com>
2026-03-26 15:47:04 +0530

    Initial Commit

---
+++ orate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  orate
+//
+//  Created by Lavish Thakkar on 26/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ orate/orateApp.swift
@@ -0,0 +1,17 @@
+//
+//  orateApp.swift
+//  orate
+//
+//  Created by Lavish Thakkar on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct orateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
